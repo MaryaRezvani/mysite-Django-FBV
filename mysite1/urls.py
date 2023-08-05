fd28ca16740dff2dcec8c3a7d9f5eb8931409f6f
MaryaRezvani <140443757+MaryaRezvani@users.noreply.github.com>
2023-08-05 13:21:57 +0330

    added general template folders
    moved website templates to general folder of templates

--- mysite1/urls.py
+++ mysite1/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-#from website.views import *
+# from website.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
