a88c484b97770d2409571c203f113ffa3bff9277
MaryaRezvani <140443757+MaryaRezvani@users.noreply.github.com>
2023-08-07 19:28:23 +0330

    Add element to post class
    change Admin
    use register decorator,
    More flexibility to access elements based on the date they were created
    by using date_hierarchy in the subclass,
    ...

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from blog.models import Post
 # Register your models here.
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    #fields =('title',)
+    list_display =('title','counted_views','status','published_date','created_date')
+    list_filter=('status',)
+    ordering=['-created_date']
+    search_fields = ['title','content']
+
+#admin.site.register(Post,PostAdmin)
