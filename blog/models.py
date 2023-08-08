ecab092165a4fcf543ccf4625e5a48ddd7d78e17
MaryaRezvani <140443757+MaryaRezvani@users.noreply.github.com>
2023-08-08 22:30:14 +0330

    add image field in model
    pip install pillow

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    #image
+    image = models.ImageField(upload_to='blog/', default = 'blog/default.jpg')
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
