a7134314677a6371d3bc47b3aa7c8305a8712a94
MaryaRezvani <140443757+MaryaRezvani@users.noreply.github.com>
2023-08-09 23:03:31 +0330

    template tag filter trunk
    trunkate text
    truncatechars

--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,8 @@ class Post(models.Model):
     def __str__(self):
         return "{} - {}".format(self.title,self.id)
         
-    
+    def snippets(self):
+        return self.content[:100] + '...'
 
 
 
