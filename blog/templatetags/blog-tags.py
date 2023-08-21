6798cbadd99163bee29363ff7f878b0027c07237
MaryaRezvani <140443757+MaryaRezvani@users.noreply.github.com>
2023-08-21 13:07:43 +0330

    Post Categories
    use inclusion tags

--- blog/templatetags/blog-tags.py
+++ blog/templatetags/blog-tags.py
@@ -1,5 +1,6 @@
 from django import template 
 from blog.models import Post
+from blog.models import Category
 register = template.Library()
 
 @register.simple_tag(name="totalpost")
@@ -20,3 +21,14 @@ def snippet(value):
 def latestposts(arg=3):
     posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+
+    return {'categories':cat_dict}
+        
