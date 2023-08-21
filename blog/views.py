5dede9f957e09f173337244f15c213ffcfe82722
MaryaRezvani <140443757+MaryaRezvani@users.noreply.github.com>
2023-08-21 22:36:09 +0330

    Pagination
    blog pagination for post

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
-
+from django.core.paginator import Paginator
 def blog_view(request,**kwargs):
     posts = Post.objects.filter(status=1)
     if kwargs.get('cat_name') != None:
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username') != None:
         posts = posts.filter(author__username = kwargs['author_username'])
+    posts = Paginator(posts,2)
+    try:    
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
