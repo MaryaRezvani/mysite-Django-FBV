248ac41035b2481b21504ee44987cb4377f4cff6
MaryaRezvani <140443757+MaryaRezvani@users.noreply.github.com>
2023-08-21 17:39:44 +0330

    merge url in veiw & category

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 
-def blog_view(request):
+def blog_view(request,cat_name=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
