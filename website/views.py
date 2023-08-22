5fd5c7e22ac2f212ad4aa85aa2d1dadf09e18571
MaryaRezvani <140443757+MaryaRezvani@users.noreply.github.com>
2023-08-22 18:44:07 +0330

    djang messsages and forms error in template
    messsage level same logging module in python

--- website/views.py
+++ website/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
 from website.models import Contact
 from website.forms import NameForm,ContactForm,NewsletterForm
+from django.contrib import messages
+
 def index_view(request):
     return render(request,'website/index.html')
 
@@ -14,6 +16,9 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request,messages.SUCCESS,'your ticket submited successfuly')
+        else:
+            messages.add_message(request,messages.ERROR,'your ticket didnt submited')
     form = ContactForm()
     return render(request,'website/contact.html',{'form':form})
 
