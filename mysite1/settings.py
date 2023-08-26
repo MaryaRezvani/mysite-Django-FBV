73010d9e37a192952c0eb906fba5119c0d60576f
MaryaRezvani <140443757+MaryaRezvani@users.noreply.github.com>
2023-08-26 22:14:51 +0330

    debugger toolbar

--- mysite1/settings.py
+++ mysite1/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'website.apps.WebsiteConfig',
     'blog',
     'accounts',
+    'debug_toolbar',
 
 
 ]
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite1.urls'
@@ -134,3 +136,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+
+]
--- mysite1/urls.py
+++ mysite1/urls.py
@@ -19,13 +19,14 @@ from django.urls import path,include
 # from website.views import *
 from django.conf import settings
 from django.conf.urls.static import static
-
+import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('url address', 'view', 'name' )
     path('accounts/',include('accounts.urls')),
     path('',include('website.urls')),
     path('blog/',include('blog.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
